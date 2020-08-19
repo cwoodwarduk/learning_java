23a1802c9ac577d866125b6a9ca725aa06c8dba5
Calum Woodward <calum@Calums-MBP.broadband>
2020-08-19 17:27:20 +0100

    completed megabytes converter challenge. Passes tests.

---
+++ s4_expressions_methods_statements_etc/megabytes_converter/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+       String mb = MegaBytesConverter.printMegaBytesAndKiloBytes(5748);
+
+       System.out.println(mb);
+    }
+}
